9c4aaf61286506d331a4adcf59d92b3f2153c423
cighao <cighao@gmail.com>
2019-07-21 15:09:35 +0300

    update

--- db/rocksdb_db.cc
+++ db/rocksdb_db.cc
@@ -28,6 +28,15 @@ int RocksDB::Delete(const std::string &table, const std::string &key){
     return 1;
 }
 
-
+int RocksDB::Reset(const std::string &table, const std::string &key){
+    db->reset_my_state();
+    rocksdb::SetPerfLevel(rocksdb::PerfLevel::kEnableTimeExceptForMutex);
+    rocksdb::get_perf_context()->Reset();
+    rocksdb::get_iostats_context()->Reset();
+    total_value_size = total_key_size = 0;
+    update_num_ = read_num_ = 0;
+    write_wal = write_thread = write_memtable = 0;
+    flush_wal_time = complete_parallel_memtable = sync_time = 0;
+}
 
 } // namespace ycsbc
--- db/rocksdb_db.h
+++ db/rocksdb_db.h
@@ -18,37 +18,77 @@
 #include "rocksdb/utilities/options_util.h"
 #include "rocksdb/utilities/transaction_db.h"
 
+#define ERR(expr) do{rocksdb::Status ec = (expr); if(!ec.ok()) fprintf(stderr,  \
+    "%s:%d: %s: %s\n", __FILE__, __LINE__, #expr, ec.ToString().c_str()), assert(false);}while(0)
+
 
 namespace ycsbc {
 
 class RocksDB : public DB {
  public:
-  RocksDB(){
+    RocksDB(){
+        
+    }
+    RocksDB(std::string path, std::string wal_dir, int logs_num):update_num_(0),
+        read_num_(0), write_wal(0), write_thread(0), write_memtable(0),
+        flush_wal_time(0), complete_parallel_memtable(0), sync_time(0),
+        total_key_size(0), total_value_size(0){
+        
+        rocksdb::Options options;
+        rocksdb::BlockBasedTableOptions table_options;
+        table_options.block_cache = rocksdb::NewLRUCache((1ul << 30) * 16, 10);
+        options.logs_num = logs_num;
+        options.wal_dir = wal_dir;
+        options.allow_concurrent_memtable_write = true;
+        options.enable_pipelined_write = false;
+        options.max_total_wal_size = (1ul << 30)*5;
+        options.table_factory.reset(rocksdb::NewBlockBasedTableFactory(table_options));
+        options.table_cache_numshardbits = 10;
+        options.IncreaseParallelism(128);
+        options.create_if_missing = true;
+        options.error_if_exists = true;
+        options.compression = rocksdb::kSnappyCompression;
+        options.bottommost_compression = rocksdb::kSnappyCompression;
+        write_options.sync = true;
+        write_options.disableWAL = false;
+        ERR(rocksdb::TransactionDB::Open(options, rocksdb::TransactionDBOptions(), path.c_str(), &db));
+        db->reset_my_state();
+    }
 
-  }
-
-  int Read(const std::string &table, const std::string &key,
+    int Read(const std::string &table, const std::string &key,
            const std::vector<std::string> *fields,
            std::vector<KVPair> &result);
 
-  int Scan(const std::string &table, const std::string &key,
+    int Scan(const std::string &table, const std::string &key,
            int len, const std::vector<std::string> *fields,
            std::vector<std::vector<KVPair>> &result) {
-    throw "Scan: function not implemented!";
-  }
+        throw "Scan: function not implemented!";
+    }
 
-  int Update(const std::string &table, const std::string &key,
+    int Update(const std::string &table, const std::string &key,
              std::vector<KVPair> &values);
 
-  int Insert(const std::string &table, const std::string &key,
+    int Insert(const std::string &table, const std::string &key,
              std::vector<KVPair> &values) {
-    return Update(table, key, values);
-  }
+        return Update(table, key, values);
+    }
 
-  int Delete(const std::string &table, const std::string &key);
+    int Delete(const std::string &table, const std::string &key);
+
+    int Reset();
 
  private:
-  rocksdb::TransactionDB *db;
+    rocksdb::TransactionDB *db;
+    rocksdb::WriteOptions write_options;
+    rocksdb::ReadOptions read_options;
+    rocksdb::Options options;
+    
+    std::string data_dir;
+    std::string wal_dir;
+    uint64_t update_num_, read_num_;
+    uint64_t write_wal, write_thread, write_memtable;
+    uint64_t flush_wal_time, complete_parallel_memtable, sync_time;
+    uint64_t total_key_size, total_value_size;
 };
 
 } // ycsbc
