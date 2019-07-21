ecbbc9b337efb095424ada85b8c8403dacdf0685
cighao <cighao@gmail.com>
2019-07-21 21:13:34 +0300

    update

--- rocksdb.cc
+++ rocksdb.cc
@@ -36,84 +36,83 @@ int main(const int argc, const char *argv[]) {
 
 
 string ParseCommandLine(int argc, const char *argv[], utils::Properties &props) {
-  int argindex = 1;
-  string filename;
-  while (argindex < argc && StrStartWith(argv[argindex], "-")) {
-    if (strcmp(argv[argindex], "-threads") == 0) {
-      argindex++;
-      if (argindex >= argc) {
-        UsageMessage(argv[0]);
+    int argindex = 1;
+    string filename;
+    while (argindex < argc && StrStartWith(argv[argindex], "-")) {
+        if (strcmp(argv[argindex], "-threads") == 0) {
+        argindex++;
+        if (argindex >= argc) {
+            UsageMessage(argv[0]);
+            exit(0);
+        }
+        props.SetProperty("threadcount", argv[argindex]);
+        argindex++;
+        }else if (strcmp(argv[argindex], "-logs") == 0) {
+        argindex++;
+        if (argindex >= argc) {
+            UsageMessage(argv[0]);
+            exit(0);
+        }
+        props.SetProperty("logs_num", argv[argindex]);
+        argindex++;
+        }else if (strcmp(argv[argindex], "-datadir") == 0) {
+        argindex++;
+        if (argindex >= argc) {
+            UsageMessage(argv[0]);
+            exit(0);
+        }
+        props.SetProperty("data_dir", argv[argindex]);
+        argindex++;
+        }else if (strcmp(argv[argindex], "-logdir") == 0) {
+        argindex++;
+        if (argindex >= argc) {
+            UsageMessage(argv[0]);
+            exit(0);
+        }
+        props.SetProperty("log_dir", argv[argindex]);
+        argindex++;
+        } else if (strcmp(argv[argindex], "-slaves") == 0) {
+        argindex++;
+        if (argindex >= argc) {
+            UsageMessage(argv[0]);
+            exit(0);
+        }
+        props.SetProperty("slaves", argv[argindex]);
+        argindex++;
+        } else if (strcmp(argv[argindex], "-P") == 0) {
+        argindex++;
+        if (argindex >= argc) {
+            UsageMessage(argv[0]);
+            exit(0);
+        }
+        filename.assign(argv[argindex]);
+        ifstream input(argv[argindex]);
+        try {
+            props.Load(input);
+        } catch (const string &message) {
+            printf("%s\n", message.c_str());
+            exit(0);
+        }
+        input.close();
+        argindex++;
+        } else {
+        printf("Unknown option  '%s'\n", argv[argindex].c_str());
         exit(0);
-      }
-      props.SetProperty("threadcount", argv[argindex]);
-      argindex++;
-    }else if (strcmp(argv[argindex], "-logs") == 0) {
-      argindex++;
-      if (argindex >= argc) {
-        UsageMessage(argv[0]);
-        exit(0);
-      }
-      props.SetProperty("logs_num", argv[argindex]);
-      argindex++;
-    }else if (strcmp(argv[argindex], "-datadir") == 0) {
-      argindex++;
-      if (argindex >= argc) {
-        UsageMessage(argv[0]);
-        exit(0);
-      }
-      props.SetProperty("data_dir", argv[argindex]);
-      argindex++;
-    }else if (strcmp(argv[argindex], "-logdir") == 0) {
-      argindex++;
-      if (argindex >= argc) {
-        UsageMessage(argv[0]);
-        exit(0);
-      }
-      props.SetProperty("log_dir", argv[argindex]);
-      argindex++;
-    } else if (strcmp(argv[argindex], "-slaves") == 0) {
-      argindex++;
-      if (argindex >= argc) {
-        UsageMessage(argv[0]);
-        exit(0);
-      }
-      props.SetProperty("slaves", argv[argindex]);
-      argindex++;
-    } else if (strcmp(argv[argindex], "-P") == 0) {
-      argindex++;
-      if (argindex >= argc) {
-        UsageMessage(argv[0]);
-        exit(0);
-      }
-      filename.assign(argv[argindex]);
-      ifstream input(argv[argindex]);
-      try {
-        props.Load(input);
-      } catch (const string &message) {
-        cout << message << endl;
-        exit(0);
-      }
-      input.close();
-      argindex++;
-    } else {
-      cout << "Unknown option '" << argv[argindex] << "'" << endl;
-      exit(0);
+        }
     }
-  }
-  if (argindex == 1 || argindex != argc) {
-    UsageMessage(argv[0]);
-    exit(0);
-  }
-  return filename;
+    if (argindex == 1 || argindex != argc) {
+        UsageMessage(argv[0]);
+        exit(0);
+    }
+    return filename;
 }
 
 void UsageMessage(const char *command) {
-  cout << "Usage: " << command << " [options]" << endl;
-  cout << "Options:" << endl;
-  cout << "  -threads n: execute using n threads (default: 1)" << endl;
-  cout << "  -db dbname: specify the name of the DB to use (default: basic)" << endl;
-  cout << "  -P propertyfile: load properties from the given file. Multiple files can" << endl;
-  cout << "                   be specified, and will be processed in the order specified" << endl;
+    printf("Usage: %s [options]\n", command);
+    printf("Options:\n");
+    printf("   -threads n: execute using n threads (default: 1)\n");
+    printf("   -P propertyfile: load properties from the given file. Multiple files can ");
+    printf("be specified, and will be processed in the order specified\n");
 }
 
 inline bool StrStartWith(const char *str, const char *pre) {
