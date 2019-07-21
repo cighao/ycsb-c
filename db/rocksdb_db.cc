a9c08ca0692de0e798b1111e4dfea1c66863269e
cighao <cighao@gmail.com>
2019-07-21 14:06:52 +0300

    update

---
+++ db/rocksdb_db.cc
@@ -0,0 +1,33 @@
+/*
+* redis_db.cc
+* Hao Chen
+* 2019-07-21
+ */
+
+#include "rocksdb_db.h"
+
+#include <cstring>
+
+using namespace std;
+
+namespace ycsbc {
+
+int RocksDB::Read(const string &table, const string &key,
+         const vector<string> *fields,
+         vector<KVPair> &result) {
+             
+    return 1;
+}
+
+int RocksDB::Update(const string &table, const string &key,
+           vector<KVPair> &values) {
+    return 1;
+}
+
+int Delete(const std::string &table, const std::string &key){
+    return 1;
+}
+
+
+
+} // namespace ycsbc
---
+++ db/rocksdb_db.h
@@ -0,0 +1,57 @@
+/*
+* redis_db.h
+* Hao Chen
+* 2019-07-21
+ */
+
+#ifndef YCSB_C_REDIS_DB_H_
+#define YCSB_C_REDIS_DB_H_
+
+#include "core/db.h"
+
+#include <iostream>
+#include <string>
+#include "core/properties.h"
+#include "rocksdb/db.h"
+#include "rocksdb/table.h"
+#include "rocksdb/options.h"
+#include "rocksdb/utilities/options_util.h"
+#include "rocksdb/utilities/transaction_db.h"
+
+
+namespace ycsbc {
+
+class RocksDB : public DB {
+ public:
+  RocksDB(){
+
+  }
+
+  int Read(const std::string &table, const std::string &key,
+           const std::vector<std::string> *fields,
+           std::vector<KVPair> &result);
+
+  int Scan(const std::string &table, const std::string &key,
+           int len, const std::vector<std::string> *fields,
+           std::vector<std::vector<KVPair>> &result) {
+    throw "Scan: function not implemented!";
+  }
+
+  int Update(const std::string &table, const std::string &key,
+             std::vector<KVPair> &values);
+
+  int Insert(const std::string &table, const std::string &key,
+             std::vector<KVPair> &values) {
+    return Update(table, key, values);
+  }
+
+  int Delete(const std::string &table, const std::string &key);
+
+ private:
+  rocksdb::TransactionDB *db;
+};
+
+} // ycsbc
+
+#endif // YCSB_C_REDIS_DB_H_
+
