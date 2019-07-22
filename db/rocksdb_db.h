a1b4b06d0e3bda5c1b7e5a2a0ded627d6e60fe45
cighao <cighao@gmail.com>
2019-07-22 14:56:49 +0300

    update

--- db/rocksdb_db.h
+++ db/rocksdb_db.h
@@ -34,17 +34,20 @@ class RocksDB{
     RocksDB(std::string path, std::string wal_dir, int logs_num):update_num_(0),
         read_num_(0), write_wal(0), write_thread_wait(0), write_memtable(0),
         flush_wal_time(0), complete_parallel_memtable(0), sync_time(0),
-        total_key_size(0), total_value_size(0){
+        total_key_size(0), total_value_size(0),
+        write_options(), read_options(){
         
-        rocksdb::Options options;
         rocksdb::BlockBasedTableOptions table_options;
         table_options.block_cache = rocksdb::NewLRUCache((1ul << 30) * 16, 10);
+        rocksdb::Options options;
+        options.table_factory.reset(rocksdb::NewBlockBasedTableFactory(table_options));
+        options.max_total_wal_size = (1ul << 30)*5;
+        options.write_buffer_size = (1ul << 30)*5;
         options.logs_num = logs_num;
         options.wal_dir = wal_dir;
         options.allow_concurrent_memtable_write = true;
         options.enable_pipelined_write = false;
         options.max_total_wal_size = (1ul << 30)*5;
-        options.table_factory.reset(rocksdb::NewBlockBasedTableFactory(table_options));
         options.table_cache_numshardbits = 10;
         options.IncreaseParallelism(128);
         options.create_if_missing = true;
@@ -79,7 +82,6 @@ class RocksDB{
     rocksdb::TransactionDB *db;
     rocksdb::WriteOptions write_options;
     rocksdb::ReadOptions read_options;
-    rocksdb::Options options;
     
     std::string data_dir;
     std::string wal_dir;
