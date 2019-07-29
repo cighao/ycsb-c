d47b079aadb126b655bd0b163d72bac8f1fa4665
cighao <cighao@gmail.com>
2019-07-29 22:20:30 +0300

    update

--- db/rocksdb_db.cc
+++ db/rocksdb_db.cc
@@ -25,6 +25,8 @@ bool RocksDB::Read(const std::string &key){
     uint64_t num = __sync_fetch_and_add(&read_num_, 1);
     if(num % 100000 == 0)
         printf("read %lu requests\n", num);
+    if(num > 999900)
+        printf("read %lu requests\n", num);
     std::string value;
     double time;
     GET_TIME_OUTPUT(time, ERR(db->Get(read_options, key, &value)));
