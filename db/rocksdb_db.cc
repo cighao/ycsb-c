e896742670c5e082079409846c279abf2f9ceda6
cighao <cighao@gmail.com>
2019-09-03 23:13:24 +0300

    update

--- db/rocksdb_db.cc
+++ db/rocksdb_db.cc
@@ -36,8 +36,8 @@ bool RocksDB::Read(const std::string &key){
 bool RocksDB::Insert(const std::string &key, std::string &value){
     //__sync_fetch_and_add(&update_num_, 1);
     uint64_t num = __sync_fetch_and_add(&update_num_, 1);
-    if(num % 100000 == 0)
-        printf("update %lu requests\n", num);
+    // if(num % 100000 == 0)
+    //     printf("update %lu requests\n", num);
     __sync_fetch_and_add(&total_key_size, key.size());
     __sync_fetch_and_add(&total_value_size, value.size());
     double time;
@@ -48,8 +48,8 @@ bool RocksDB::Insert(const std::string &key, std::string &value){
 
 bool RocksDB::Delete(const std::string &key){
     uint64_t num = __sync_fetch_and_add(&update_num_, 1);
-    if(num % 100000 == 0)
-        printf("update %lu requests\n", num);
+    // if(num % 100000 == 0)
+    //     printf("update %lu requests\n", num);
     double time;
     GET_TIME_OUTPUT(time, ERR(db->Delete(write_options, key)));
     update_time_.insert(time);
